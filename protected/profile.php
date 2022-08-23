d916661a98a76858a818008fde4a98d2f9ef0cb1
Indiedude78 <saj39@njit.edu>
2022-08-23 17:22:03 -0400

    Fixed issue with CORS preflight

--- protected/profile.php
+++ protected/profile.php
@@ -1,4 +1,7 @@
 <?php
+
+//set headers
+require_once(__DIR__ . "/../lib/headers.php");
 require_once(__DIR__ . "/../lib/db.php");
 require_once(__DIR__ . "/../lib/secret_key.php");
 require_once(__DIR__ . "/../vendor/autoload.php");
@@ -7,18 +10,14 @@ use \Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 error_reporting(E_ERROR);
-//set headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-HEADER("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 //define key
 $secret_key = $key;
-$jwt = null;
 $data = json_decode(file_get_contents("php://input"));
-$jwt = isset($data->token) ? $data->token : null;
+$jwt = null;
+$jwt = $data->token;
+
+//$jwt = isset($data->token) ? $data->token : null;
 if ($jwt) {
     try {
         $decoded = JWT::decode($jwt, new Key($secret_key, 'HS256'));
@@ -46,7 +45,7 @@ if ($jwt) {
     http_response_code(401);
     echo json_encode(array(
         "message" => "Access denied.",
-        "error" => "No token provided."
+        "error" => "No token provided.",
     ));
     $isValid = false;
 }
